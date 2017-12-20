dc415cbd9973edb711d07393d07f091069972f73
Robert Beekman <spoinky.nl@gmail.com>
2017-12-20 13:12:59 +0100

    More corpus data

---
+++ csharp/Willy.Core/Services/WillyChatService.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace Willy.Core.Services
+{
+    public class WillyChatService : IWillyChatService
+    {
+        public WillyChatService()
+        {
+            // Create an instance of the Python Chatterbot
+            Chatterbot = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+        }
+
+        public Process Chatterbot { get; set; }
+    }
+
+    public interface IWillyChatService
+    {
+    }
+}
